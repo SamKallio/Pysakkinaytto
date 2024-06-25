80b9ba5cfda4613d49800d203f04d5f80cd9771d
SamKallio <sam.kalliomaki@gmail.com>
2024-06-25 22:03:00 +0300

    implemented SearchStopData component and little bit of css tuning

--- src/components/BusStopScreen.jsx
+++ src/components/BusStopScreen.jsx
@@ -1,10 +1,12 @@
 import "../App.css";
+import SearchStopData from "./SearchStopData";
 
 function BusStopScreen() {
   return (
     <div className="container">
       <h2 id="stopHeader">Pysäkkinäyttö</h2>
       <div id="stopScreen"></div>
+      <SearchStopData />
     </div>
   );
 }
---
+++ src/components/SearchStopData.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import { useState } from "react";
+
+function SearchStopData() {
+  const [currentNumber, setCurrentNumber] = useState(0);
+
+  const searchStop = (e) => {
+    e.preventDefault();
+
+    if (currentNumber) {
+      fetch("http://data.foli.fi/siri/sm/pretty")
+        .then((response) => {
+          if (!response.ok) {
+            throw new Error("Network response was not ok");
+          }
+          return response.json();
+        })
+        .then((data) => {
+          if (data.hasOwnProperty(currentNumber)) {
+            const stopData = data[currentNumber];
+            console.log(
+              "stopNumber: " +
+                currentNumber +
+                " stopName: " +
+                stopData.stop_name
+            );
+          } else {
+            console.log("Couldn't find data for this stop number");
+          }
+        })
+        .catch((error) => {
+          console.error("Error while fetching:", error);
+        });
+    }
+  };
+
+  return (
+    <form id="searchBox" onSubmit={searchStop}>
+      <label>Pysäkkinumero:</label>
+      <input
+        id="searchInput"
+        type="number"
+        min="0"
+        onChange={() => setCurrentNumber(event.target.value)}
+      ></input>
+      <input type="submit" value="Etsi"></input>
+    </form>
+  );
+}
+
+export default SearchStopData;
